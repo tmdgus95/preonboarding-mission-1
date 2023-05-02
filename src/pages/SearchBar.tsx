3ea5a635a90d83ba196ec812e9966ade2e9f24ba
tmdgus95 <8571053@naver.com>
2023-05-03 01:17:38 +0900

    프록시 설정

--- src/pages/SearchBar.tsx
+++ src/pages/SearchBar.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 import { KeyWordSearchInstance } from '../api/search';
+import axios from 'axios';
 
 export default function SearchBar() {
   useEffect(() => {
-    KeyWordSearchInstance.get('api/v1/search-conditions/?name=갑상선')
-      .then((res) => console.log(res))
+    KeyWordSearchInstance.get('/api/v1/search-conditions/?name=갑상')
+      .then(console.log)
       .catch(console.log);
   }, []);
+
   return <div>SearchBar</div>;
 }
