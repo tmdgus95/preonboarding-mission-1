31c1e6a97bb156e9f528b0b29e50e45fa083e08a
tmdgus95 <8571053@naver.com>
2023-05-03 01:17:30 +0900

    api 수정

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const KeyWordSearchInstance = axios.create({
-  baseURL: process.env.REACT_APP_SEARCH_URL,
+  baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
   },
